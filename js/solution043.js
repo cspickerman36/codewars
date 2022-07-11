19dc361e8981307d8ab7492c13b5ec8c1a6cf92f
cspickerman36 <spicktopher@gmail.com>
2022-07-10 21:37:47 -0400

    Codewars

---
+++ js/solution043.js
@@ -0,0 +1,3 @@
+// No Loops 2 - You only need one: You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+let check = (a,x) => a.includes(x); 
