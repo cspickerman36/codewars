629e3f9925d0ffdd5438f842fd5bfd33821794c6
cspickerman36 <spicktopher@gmail.com>
2022-07-16 22:07:08 -0400

    Codewars

---
+++ js/solution047.js
@@ -0,0 +1,13 @@
+// Basic Mathematical Operations: Your task is to create a function that does four basic mathematical operations.
+
+function basicOp(operation, value1, value2){
+    if (operation == '+'){
+      return value1 + value2
+    }else if (operation == '-'){
+      return value1 - value2
+   }else if (operation == '*'){
+      return value1 * value2
+     }else{
+       return value1/value2
+     }
+   }
