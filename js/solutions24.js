25c8bd7b86ac17e344b2512c4dad6ce974fee8cf
cspickerman36 <spicktopher@gmail.com>
2022-07-21 20:33:14 -0400

    Finished 024

---
+++ js/solutions24.js
@@ -0,0 +1,9 @@
+// Convert a Boolean to a String: Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    if (b == true){
+      return "true"
+    }else{
+      return "false"
+    }
+  }
