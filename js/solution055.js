c8b424fc133de062313a7b3347f34062ef1f7f2e
cspickerman36 <spicktopher@gmail.com>
2022-08-01 20:48:01 -0400

    Codewars

---
+++ js/solution055.js
@@ -0,0 +1,5 @@
+// No Loops 1 - Small enough?: You will be given an array (a) and a limit value (limit). You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+function smallEnough(a, limit) {
+    return a.every(x => x <= limit);
+  }
