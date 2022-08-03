ab9fd8e39c305473f6c90395c0ffeb06745ebb11
cspickerman36 <spicktopher@gmail.com>
2022-08-02 20:51:28 -0400

    Codewars

---
+++ js/solution056.js
@@ -0,0 +1,20 @@
+// Count of positives / sum of negatives: Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
