bf6489278a08e43cd8ffd827cb3439a41b3494d1
cspickerman36 <spicktopher@gmail.com>
2022-08-08 21:26:05 -0400

    codewars

---
+++ js/solution061.js
@@ -0,0 +1,5 @@
+// isReallyNaN: Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+
+const isReallyNaN = (val) => {
+    return val !== val;
+   }
