012c2e010815009992f9f585b549b564b000fffa
cspickerman36 <spicktopher@gmail.com>
2022-08-12 21:36:10 -0400

    codewars~

---
+++ js/solution064.js
@@ -0,0 +1,5 @@
+// Capitalization and Mutability: Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+function capitalizeWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+     }
