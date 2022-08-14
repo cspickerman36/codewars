174928cdecf8700e55a6677dadbbc3337e1fdea5
cspickerman36 <spicktopher@gmail.com>
2022-08-13 21:38:15 -0400

    codewars~

---
+++ js/solution065.js
@@ -0,0 +1,7 @@
+// Take the Derivative: Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+function derive(coefficient,exponent) {
+    let coe = coefficient*exponent
+    let expo = exponent-1
+    return `${coe}x^${expo}`
+  }
