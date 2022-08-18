c149972abce1ba89783b4e7c8026f8242a548555
cspickerman36 <spicktopher@gmail.com>
2022-08-18 14:28:59 -0400

    codewars~

---
+++ js/solution075.js
@@ -0,0 +1,7 @@
+//Sum The Strings: Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+  
+    return (parseInt(~~a,10)+parseInt(~~b,10)).toString()
+    
+  }
