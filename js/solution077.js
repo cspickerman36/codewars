95995b078d75b2db8340449d235a66b3de50f892
cspickerman36 <spicktopher@gmail.com>
2022-08-20 22:25:42 -0400

    codewars~

--- js/solution077.js
+++ js/solution077.js
@@ -1,4 +1,4 @@
-//Palindrome Strings: Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+//Palindrome Strings: Implement a function that checks if something is a palindrome. If the input is a number, then convert it to string first.
 
 function isPalindrome(line) {
     let str = line.toString()
