3c327761cbb54571b89a0e61138be17edbc15209
cspickerman36 <spicktopher@gmail.com>
2022-08-22 22:58:23 -0400

    codewars~

---
+++ js/solution078.js
@@ -0,0 +1,6 @@
+//Grasshopper - Combine strings: Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames(Pram, Dram){
+    const ary = [Pram,Dram]
+    return ary.join(' ')
+    }
