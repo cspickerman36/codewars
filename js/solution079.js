91458a83d0eb6fc0b3ac3d7e64083ca35a5e7f9c
cspickerman36 <spicktopher@gmail.com>
2022-08-23 23:01:03 -0400

    codewars~

---
+++ js/solution079.js
@@ -0,0 +1,5 @@
+//Template Strings: Your task is to return the correct string using the Template String Feature.
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} ${'are'} ${feature}`
+  }
