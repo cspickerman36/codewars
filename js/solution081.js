5c8860852c58fbe834e901e6390ab06fcc513db7
cspickerman36 <spicktopher@gmail.com>
2022-08-25 23:06:58 -0400

    codewars~

---
+++ js/solution081.js
@@ -0,0 +1,11 @@
+//Exclamation marks series #1: Remove an exclamation mark from the end of string: Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+function remove (string) {
+
+    if(string.endsWith("!")) {
+    return string.substring(0, string.length - 1)
+    }else{
+       return string
+      }
+     }
+
