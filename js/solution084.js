2cd483958e17630eea4d86c34ca44b465585352e
cspickerman36 <spicktopher@gmail.com>
2022-08-28 23:18:05 -0400

    codewars~

---
+++ js/solution084.js
@@ -0,0 +1,7 @@
+//Sum Mixed Array: Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+  }
+
+  
