1631aa73494fdf7337f8b5c5c680bac64c0fa7c3
cspickerman36 <spicktopher@gmail.com>
2022-09-01 14:06:13 -0400

    codewars~

---
+++ js/solution068.js
@@ -0,0 +1,7 @@
+// Template Strings: You must output a string containing the two strings with the word ```' are '```
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} ${'are'} ${feature}`
+  }
+
+  
